8b0cf1c0eb502511d24a57e8c4e6e54774d746b3
Jelili Adesina <jelilio.adesina@gmail.com>
2023-05-28 09:40:29 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.app_context()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'An error occurred'
+        pass
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task_to_delete = Todo.query.get_or_404(task_id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'An error occurred while deleting'
+
+
+@app.route('/update/<int:task_id>', methods=['POST', 'GET'])
+def update(task_id):
+    task_to_update = Todo.query.get_or_404(task_id)
+
+    if request.method == 'POST':
+        task_content = request.form['content']
+        task_to_update.content = task_content
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'An error occurred while updating'
+        pass
+    else:
+        return render_template("update.html", task=task_to_update)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
